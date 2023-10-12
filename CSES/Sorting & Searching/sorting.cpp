b6dd5479750d8a3b1a638aada03dca0f8f007f4b
Farhan-FMS <b190305043@cse.jnu.ac.bd>
2023-10-12 12:35:40 +0600

    sorting

---
+++ CSES/Sorting & Searching/sorting.cpp
@@ -0,0 +1,35 @@
+//Farhan404
+#include <bits/stdc++.h>
+using namespace std;
+#define GET_FASTER ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+typedef long long int ll;
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define all(x) begin(x), end(x)
+#define ld double
+const int N = 1e6 + 5;
+const ll MOD = 1e9 + 7;
+const ll mx = LLONG_MAX;
+const ll mn = LLONG_MIN;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void solve() {
+	
+
+}
+int main()
+{
+	GET_FASTER;
+	int t = 1;
+	//cin >> t;
+	for (ll i = 1; i <= t; i++) {
+		//cout << "Case " << i << ": ";
+		solve();
+	}
+	return 0;
+}
+
