5e794559dd0d5782bbbb197d93be3a0e9d151d9d
fms-byte <fsh69711@gmail.com>
2023-12-11 12:26:14 +0600

    lc-1 update

--- LeetCode/Daily Problem/11 Dec 1287_More_than_25%.cpp
+++ LeetCode/Daily Problem/11 Dec 1287_More_than_25%.cpp
@@ -1,42 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
         int len = arr.size();
-        if(len == 1){
+        if (len == 1) {
             return 1;
         }
 
-        int number= arr[0], cnt = 1, ans=-1;
-        for(int i=1;i<len;i++){
-            if(arr[i-1]==arr[i]){
+        int number = arr[0], cnt = 1, ans = -1;
+        for (int i = 1; i < len; i++) {
+            if (arr[i - 1] == arr[i]) {
                 cnt++;
-                if(cnt>ans){
-                    number=arr[i];
+                if (cnt > ans) {
+                    number = arr[i];
                     ans = cnt;
                 }
-            }else{
-                cnt=1;
+            } else {
+                cnt = 1;
             }
         }
-        if((float)ans > ((float)len/4.0)){
+        if ((float)ans > ((float)len / 4.0)) {
             return number;
-        }else{
+        } else {
             return -1;
         }
     }
-
-    void main(){
-        vector<int> arr;
-        int n;
-        while (cin >> n) { //1 2 2 6 6 6 6 7 10
-            arr.push_back(n);
-            if (cin.get() == '\n') {
-                break;
-            }
-        }
-
-        int ans = findSpecialInteger(arr);
-        if(ans != -1)
-            cout<<ans<<endl;
-    }
 };
+
+int main() {
+    Solution solution;
+    vector<int> arr;
+    int n;
+    while (cin >> n) {
+        arr.push_back(n);
+        if (cin.get() == '\n') {
+            break;
+        }
+    }
+
+    int ans = solution.findSpecialInteger(arr);
+    if (ans != -1)
+        cout << ans << endl;
+
+    return 0;
+}
