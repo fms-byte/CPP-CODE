fe0708a345300b60f2b75be08b95edb747699855
fms-byte <fsh69711@gmail.com>
2023-12-25 00:42:53 +0600

    Update Template

--- template.cpp
+++ template.cpp
@@ -1,25 +1,40 @@
-//Farhan404
 #include <bits/stdc++.h>
 using namespace std;
-#define GET_FASTER ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define endl '\n'
-#define nline cout<<'\n'
+#define GET_FASTER 		ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl 			'\n'
+#define nline 			cout<<'\n'
 typedef long long int ll;
 typedef double ld;
+typedef unsigned int uint;
 typedef vector<int> vi;
+typedef vector<long long> vlli;
 typedef pair<int, int> pi;
-#define fi first
-#define se second
-#define pb push_back
-#define mp make_pair
-#define all(x) begin(x), end(x)
-#define arr_sort(a) sort(a, a+n);
-#define vec_sort(v) sort(v.begin(),v.end());
-#define Rev(v) reverse(v.begin(),v.end());
+#define fi 				first
+#define se 				second
+#define pb 				push_back
+#define mp 				make_pair
+#define all(x) 			begin(x), end(x)
+#define arr_sort(a) 	sort(a, a+n);
+#define vec_sort(v) 	sort(v.begin(),v.end());
+#define Rev(v) 			reverse(v.begin(),v.end());
+#define show(n) 		cout << n << endl;
+#define yes 			show("YES");
+#define no 				show("NO");
+#define loop(i,n)      	for (int i = 0; i < n; i++)
+#define forr(i,a,b)    	for(ll i=a;i<=b;i++)
 
-const ll MOD = 1e9 + 7;
+const ll mod = 1e9 + 7;
 const ll mx = LLONG_MAX;
 const ll mn = LLONG_MIN;
+
+ll mul_inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * mul_inv(mod % i)) % mod) % mod;}
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll lcm(ll a, ll b) {return (a * b) / gcd(a, b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = (res * a) % mod; a = (a * a ) % mod; b >>= 1;} return res;}
+
 /*
 int dp[N][7];
 
@@ -56,6 +71,28 @@ void primeFactors(int n) //factorization (divisors of n)
 	}
 	cout << n << endl;
 	cout << cnt << endl;
+}
+
+vector<ll> all_divisor(ll n, ll l) { //factorization (better soln)
+	vector<ll> divisors;
+	if (l < 2)
+		divisors.pb(1);
+
+	for (ll i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			if (i > l) {
+				divisors.pb(i);
+			}
+			ll j = n / i;
+			if (j != i) {
+				if (j > l)
+					divisors.pb(j);
+			}
+		}
+	}
+
+	divisors.pb(n);
+	return divisors;
 }
 
 bool is_prime(int n) {
