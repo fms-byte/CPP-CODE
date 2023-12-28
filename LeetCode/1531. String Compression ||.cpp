94a2f53e95d61a5a004d4ae34a62465a53ffaac4
Farhan-FMS <61820314+fms-byte@users.noreply.github.com>
2023-12-28 16:24:55 +0600

    Update and rename 1531. String Compression.cpp to 1531. String Compression ||.cpp

--- LeetCode/1531. String Compression ||.cpp
+++ LeetCode/1531. String Compression ||.cpp
@@ -23,12 +23,12 @@ public:
             }
         }
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
-        }
+        // for (int i = 0; i <= n; i++) {
+        //     for (int j = 0; j <= k; j++) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
         return dp[n][k];
     }
 };
